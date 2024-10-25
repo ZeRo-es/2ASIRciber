30e28f2be056e9448b6c753b4727dfcf5469a31e
luis <luis@luis.com>
2024-10-25 12:57:48 +0200

    commit nuevo

---
+++ CopiasdeSeguridad/copiads.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Este script crea copias de seguridad
+
+echo "Bienvenido al script de copias de seguridad"
+
+echo -e "Abriendo ficheros [ \e[32mOK\e[0m ]"
+echo -e "Abriendo Bases de Datos[ \e[31mERROR\e[0m ]"
+
+cp 
---
+++ CopiasdeSeguridad/readme.txt
@@ -0,0 +1,4 @@
+sudo mkdir /etc/cosas_importantes
+cd /etc/cosas_importantes
+sudo touch {cosa1,cosa2,cosa3}
+sudo nano copiads.sh
